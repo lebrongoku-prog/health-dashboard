bda222ae76ac5ed64aba48e4bd7c8389158bfa6e
lebrongoku-prog <leonhaz505@gmail.com>
2026-05-25 01:01:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //    'hcc-v1' → 'hcc-v2' → 'hcc-v3' …
 // ═══════════════════════════════════════════════════════════
 
-const CACHE = 'hcc-v11';
+const CACHE = 'hcc-v12';
 
 // Lokale Shell-Assets, die vorab gecacht werden
 const ASSETS = [
